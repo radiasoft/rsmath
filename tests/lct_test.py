1b95fe9a9c54749d1bcb6ce7b67cdc24f21f84e0
gurhar1133 <ghns1133@gmail.com>
2022-10-05 20:42:35 +0000

    Fix #4: test WIP

--- tests/lct_test.py
+++ tests/lct_test.py
@@ -1,10 +1,14 @@
 from rsmath import lct_lib
 from pykern import pkunit
+from pykern.pkcollections import PKDict
 from pykern import pkio
 import numpy as np
 
 
-def test_lct_abscissae():
+# TODO (gurhar1133): floating point not allways deterministic
+# change test structure to reflect that fact
+
+def test_lct_abscissae_and_signal():
 
     def testFn1(u):
         return np.exp(- np.pi * (1. + 1.j) * u**2)
@@ -46,47 +50,44 @@ def test_lct_abscissae():
         if np.isscalar(u): rs = rs[0]
         return rs
 
-    rh1 = 3.
-    np1 = 69
-    du1 = rh1 / (np1 // 2)
-    uvals1 = lct_lib.lct_abscissae(np1, du1)
-    fvals1 = testFn1(uvals1)
-    fvals1a = testFn1a(uvals1)
+    def d_f_u_vals(rh, np, fn_idx):
+        du = rh / (np // 2)
+        uvals = lct_lib.lct_abscissae(np, du)
+        fvals = [
+            testFn1,
+            testFn2,
+            testFn3,
+            lambda x: [ testFn4(u) for u in x ],
+        ][fn_idx](uvals)
+        return du, uvals, fvals
 
-    rh2 = 5.
-    np2 = 50
-    du2 = rh2 / (np2 // 2)
-    uvals2 = lct_lib.lct_abscissae(np2, du2)
-    fvals2 = testFn2(uvals2)
+    dus = []
+    all_fvals = []
+    all_uvals = []
+    for i, inputs in enumerate(((3., 69), (5., 50), (8., 100), (8., 100))):
+        du, uvals, fvals = d_f_u_vals(*inputs, i)
+        dus.append(du)
+        all_fvals.append(fvals)
+        all_uvals.append(uvals)
 
-    rh3 = 8.
-    np3 = 100
-    du3 = rh3 / (np3 // 2)
-    uvals3 = lct_lib.lct_abscissae(np3, du3)
-    fvals3 = testFn3(uvals3)
-
-    rh4 = 8.
-    np4 = 100
-    du4 = rh4 / (np4 // 2)
-    uvals4 = lct_lib.lct_abscissae(np4, du4)
-    fvals4 = [ testFn4(u) for u in uvals4 ]
-
-    du1, du2, du3, du4
-
-    assert (du1, du2, du3, du4) == (0.08823529411764706, 0.2, 0.16, 0.16)
+    assert dus == [0.08823529411764706, 0.2, 0.16, 0.16]
 
     for d in pkunit.case_dirs(group_prefix="1"):
         pkio.write_text(d.join("u_and_f_vals.txt"),
-            str(
-                [
-                    uvals1,
-                    uvals2,
-                    uvals3,
-                    uvals4,
-                    fvals1,
-                    fvals2,
-                    fvals3,
-                    fvals4,
-                ]
+            str([*all_uvals, *all_fvals]
             )
         )
+
+    assert list(lct_lib.lct_abscissae(8, 0.25)) == [-1.  , -0.75, -0.5 , -0.25,  0.  ,  0.25,  0.5 ,  0.75]
+    assert list(lct_lib.lct_abscissae(7, 0.25)) == [-0.75, -0.5 , -0.25,  0.  ,  0.25,  0.5 ,  0.75]
+    assert list(lct_lib.lct_abscissae(8, 0.25, ishift = True)) == [ 0.  ,  0.25,  0.5 ,  0.75, -1.  , -0.75, -0.5 , -0.25]
+    assert list(lct_lib.lct_abscissae(7, 0.25, ishift = True)) == [ 0.  ,  0.25,  0.5 ,  0.75, -0.75, -0.5 , -0.25]
+    assert [round(x, 1) for x in list(lct_lib.lct_abscissae(20, 3 / (20 // 2)))] == [-3. , -2.7, -2.4, -2.1, -1.8, -1.5, -1.2, -0.9, -0.6, -0.3,  0. , 0.3,  0.6,  0.9,  1.2,  1.5,  1.8,  2.1,  2.4,  2.7]
+    assert [round(x, 1) for x in list(lct_lib.lct_abscissae(21, 3 / (21 // 2)))] == [-3. , -2.7, -2.4, -2.1, -1.8, -1.5, -1.2, -0.9, -0.6, -0.3,  0. , 0.3,  0.6,  0.9,  1.2,  1.5,  1.8,  2.1,  2.4,  2.7,  3. ]
+
+    k_rsmp = 2.0
+    signals = list(zip(dus, all_fvals))
+    rsmps = [lct_lib.resample_signal(k_rsmp, sig) for sig in signals]
+
+    for d in pkunit.case_dirs(group_prefix="2"):
+        pkio.write_text(d.join("signals.txt"), str(rsmps))

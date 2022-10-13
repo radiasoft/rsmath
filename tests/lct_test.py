9457f7f0b3b0fc3191a782cd4d16d449829887e3
gurhar1133 <ghns1133@gmail.com>
2022-10-13 16:41:17 +0000

    Fix #5: more test

--- tests/lct_test.py
+++ tests/lct_test.py
@@ -9,68 +9,12 @@ import re
 
 _K_RSMP = 2.0
 _M_SCL = -2
+_Q_CM = 0.667
 
 def test_lct_signal():
     data_dir = pkunit.data_dir()
     work_dir = pkunit.empty_work_dir()
-
-    def testFn1(u):
-        return np.exp(- np.pi * (1. + 1.j) * u**2)
-
-    def tri(t):
-        if type(t) in (list, np.ndarray):
-            t_a = np.array(t)
-            oz = [ 1 if b1 and b2 else 0 for b1, b2 in zip(-1 < t_a, t_a < 1) ]
-            r = np.array(oz) * (1 - np.sign(t_a) * t_a )
-        else:
-            r = 0
-            if -1 < t and t < 1: r = 1 - np.sign(t) * t
-        return r
-
-    def testFn2(u):
-        return 1.5 * tri(u / 3.) - 0.5 * tri(u)
-
-    def testFn3(u):
-        bit = [0, 1, 1, 0, 1, 0, 1, 0]
-        nn = len(bit)
-        idx = np.floor(4 + u / 2).astype(int)
-        return [ bit[i] if 0 <= i and i < nn else 0 for i in idx ]
-
-    def testFn4(u):
-        ua = np.ndarray.flatten(np.asarray(u))
-        xv = [ -6, -5, -4,  -3.5, -3, -2, -1, 0, 1, 1.5, 2.5, 2.5, 3.5, 3.5, 4,   5, 6 ]
-        yv = [  0,  1,  0.5, 0.5,  1,  1,  2, 1, 1, 0.5, 0.5, 1,   1,   0.5, 0.5, 1, 0 ]
-        rs = np.zeros(len(ua))
-        indices = [ [ i for i,x in enumerate(xv) if x < ue ] for ue in ua ]
-        indices = [ (i, ii[-1]) for i,ii in enumerate(indices) if 0 < len(ii) and len(ii) < len(xv) ]
-        for idx in indices:
-            x0, x1 = xv[idx[1] : idx[1] + 2]
-            y0, y1 = yv[idx[1] : idx[1] + 2]
-            dx = x1 - x0
-            rs[idx[0]] = y0 * (x1 - ua[idx[0]]) / dx + y1 * (ua[idx[0]] - x0) / dx
-        if np.isscalar(u): rs = rs[0]
-        return rs
-
-    def d_f_u_vals(rh, np, fn_idx):
-        du = rh / (np // 2)
-        uvals = lct.lct_abscissae(np, du)
-        fvals = [
-            testFn1,
-            testFn2,
-            testFn3,
-            lambda x: [ testFn4(u) for u in x ],
-        ][fn_idx](uvals)
-        return du, uvals, fvals
-
-    dus = []
-    all_fvals = []
-    all_uvals = []
-    for i, inputs in enumerate(((3., 69), (5., 50), (8., 100), (8., 100))):
-        du, uvals, fvals = d_f_u_vals(*inputs, i)
-        dus.append(du)
-        all_fvals.append([f for f in fvals])
-        all_uvals.append([u for u in uvals])
-
+    dus, all_fvals, all_uvals = _get_vals()
     sigs = list(zip(dus, all_fvals))
     f = _fmt_matrix_string(str([
         [_cast_complex_for_write(d) for d in dus],
@@ -79,7 +23,6 @@ def test_lct_signal():
     ]))
     s = _cast_from_complex_signal(sigs, lct.resample_signal, _K_RSMP)
     c = _cast_from_complex_signal(sigs, lct.scale_signal, _M_SCL)
-
     for case in (
         PKDict(case="u_and_f_vals", data=f),
         PKDict(case="signals", data=s),
@@ -116,19 +59,68 @@ def test_lct_abscissae():
 
 
 def test_lct_fourier():
-    pass
+    data_dir = pkunit.data_dir()
+    work_dir = pkunit.empty_work_dir()
+    dus, all_fvals, all_uvals = _get_vals()
+    sigs = list(zip(dus, all_fvals))
+    r = _cast_from_complex_signal(sigs, lct.lct_fourier, None)
+    _ndiff_files(
+        data_dir.join("fourier.txt"),
+        pkio.write_text(work_dir.join("fourier_actual.txt"),
+            r
+        ),
+        work_dir.join("ndiff.out"),
+        data_dir
+    )
 
 
 def test_chirp_multiply():
-    pass
+    data_dir = pkunit.data_dir()
+    work_dir = pkunit.empty_work_dir()
+    dus, all_fvals, all_uvals = _get_vals()
+    sigs = list(zip(dus, all_fvals))
+    r = _cast_from_complex_signal(sigs, lct.chirp_multiply, _Q_CM)
+    _ndiff_files(
+        data_dir.join("cm_signals.txt"),
+        pkio.write_text(work_dir.join("cm_signals_actual.txt"),
+            r
+        ),
+        work_dir.join("ndiff.out"),
+        data_dir
+    )
 
 
 def test_lct_decomposition():
-    pass
+    data_dir = pkunit.data_dir()
+    work_dir = pkunit.empty_work_dir()
+    ms = [[[ 0.5, -0.5], [ 0.5,  1.5]],
+         [[ 2.,   1. ], [-2.6, -0.8]],
+         [[-0.5,  0.5], [-0.5, -1.5]]]
+    d = str([lct.lct_decomposition(m) for m in ms])
+    _ndiff_files(
+        data_dir.join("lct_decomp.txt"),
+        pkio.write_text(work_dir.join("lct_decomp_actual.txt"),
+            d
+        ),
+        work_dir.join("ndiff.out"),
+        data_dir
+    )
 
 
 def test_apply_lct():
     pass
+
+
+def _get_vals():
+    dus = []
+    all_fvals = []
+    all_uvals = []
+    for i, inputs in enumerate(((3., 69), (5., 50), (8., 100), (8., 100))):
+        du, uvals, fvals = d_f_u_vals(*inputs, i)
+        dus.append(du)
+        all_fvals.append([f for f in fvals])
+        all_uvals.append([u for u in uvals])
+    return dus, all_fvals, all_uvals
 
 
 def _ndiff_files(expect_path, actual_path, diff_file, data_dir):
@@ -146,7 +138,10 @@ def _cast_complex_for_write(number):
 
 
 def _cast_from_complex_signal(signals, signal_function, factor):
-        s = [signal_function(factor, sig) for sig in signals]
+        if factor:
+            s = [signal_function(factor, sig) for sig in signals]
+        else:
+            s = [signal_function(sig) for sig in signals]
         for arr in s:
             conv = [_cast_complex_for_write(number) for number in arr[1]]
             arr[1] = conv
@@ -155,3 +150,57 @@ def _cast_from_complex_signal(signals, signal_function, factor):
 
 def _fmt_matrix_string(matrix_string):
     return matrix_string.replace("],", "]\n")
+
+
+def _fn1(u):
+    return np.exp(- np.pi * (1. + 1.j) * u**2)
+
+
+def _tri(t):
+    if type(t) in (list, np.ndarray):
+        t_a = np.array(t)
+        oz = [ 1 if b1 and b2 else 0 for b1, b2 in zip(-1 < t_a, t_a < 1) ]
+        r = np.array(oz) * (1 - np.sign(t_a) * t_a )
+    else:
+        r = 0
+        if -1 < t and t < 1: r = 1 - np.sign(t) * t
+    return r
+
+
+def _fn2(u):
+    return 1.5 * _tri(u / 3.) - 0.5 * _tri(u)
+
+
+def _fn3(u):
+    bit = [0, 1, 1, 0, 1, 0, 1, 0]
+    nn = len(bit)
+    idx = np.floor(4 + u / 2).astype(int)
+    return [ bit[i] if 0 <= i and i < nn else 0 for i in idx ]
+
+
+def _fn4(u):
+    ua = np.ndarray.flatten(np.asarray(u))
+    xv = [ -6, -5, -4,  -3.5, -3, -2, -1, 0, 1, 1.5, 2.5, 2.5, 3.5, 3.5, 4,   5, 6 ]
+    yv = [  0,  1,  0.5, 0.5,  1,  1,  2, 1, 1, 0.5, 0.5, 1,   1,   0.5, 0.5, 1, 0 ]
+    rs = np.zeros(len(ua))
+    indices = [ [ i for i,x in enumerate(xv) if x < ue ] for ue in ua ]
+    indices = [ (i, ii[-1]) for i,ii in enumerate(indices) if 0 < len(ii) and len(ii) < len(xv) ]
+    for idx in indices:
+        x0, x1 = xv[idx[1] : idx[1] + 2]
+        y0, y1 = yv[idx[1] : idx[1] + 2]
+        dx = x1 - x0
+        rs[idx[0]] = y0 * (x1 - ua[idx[0]]) / dx + y1 * (ua[idx[0]] - x0) / dx
+    if np.isscalar(u): rs = rs[0]
+    return rs
+
+
+def d_f_u_vals(rh, np, fn_idx):
+    du = rh / (np // 2)
+    uvals = lct.lct_abscissae(np, du)
+    fvals = [
+        _fn1,
+        _fn2,
+        _fn3,
+        lambda x: [ _fn4(u) for u in x ],
+    ][fn_idx](uvals)
+    return du, uvals, fvals

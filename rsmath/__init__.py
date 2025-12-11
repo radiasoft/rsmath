58dcd7af433227f867a832046a4cf2b3cae310aa
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2025-12-11 23:02:17 +0000

    Fix #16 pyproject.toml

--- rsmath/__init__.py
+++ rsmath/__init__.py
@@ -1,14 +1,12 @@
-# -*- coding: utf-8 -*-
-u""":mod:`rsmath` package
+""":mod:`rsmath` package
 
-:copyright: Copyright (c) 2022 RadiaSoft LLC.  All Rights Reserved.
-:license: http://www.apache.org/licenses/LICENSE-2.0.html
+:copyright: Copyright (c) 2025 RadiaSoft LLC.  All Rights Reserved.
+:license: https://www.apache.org/licenses/LICENSE-2.0.html
 """
-from __future__ import absolute_import, division, print_function
-import pkg_resources
+import importlib.metadata
 
 try:
     # We only have a version once the package is installed.
-    __version__ = pkg_resources.get_distribution('rsmath').version
-except pkg_resources.DistributionNotFound:
+    __version__ = importlib.metadata.version("rsmath")
+except importlib.metadata.PackageNotFoundError:
     pass
